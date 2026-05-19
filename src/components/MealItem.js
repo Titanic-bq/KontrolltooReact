a8505251eb5c2ccdecea438ad9d8a5ae87a0dee0
Bruno.Rõzov <bruno.rozov@voco.ee>
2026-05-19 14:03:58 +0300

    Ülesanne 5 - Valuuta andmete kuvamine Mealitems.js

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -10,7 +10,12 @@ const MealItem = (props) => {
         />
         <div>
           <h3>{props.meal.name}</h3>
-          <p className="meal-item-price">{props.meal.price}</p>
+         <p className="meal-item-price">
+            {new Intl.NumberFormat("de-DE", {
+              style: "currency",
+              currency: "EUR",
+            }).format(props.meal.price)}
+          </p>
           <p className="meal-item-description">{props.meal.description}</p>
         </div>
         <p>
