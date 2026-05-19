91bc4701f62c74d0fdb4f49e4b18ec6774a045cf
Titanic-bq <hendrik.jurgenson@voco.ee>
2026-05-19 08:28:17 +0000

    1.Header and Meals komponentide lisamine rakendusele

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import Meals from "./components/Meals";
+import Header from "./components/Header";
+
+
 const App = () => {
  return (
     <>
+    <Header></Header>
+    <Meals></Meals>
      <h1>Food Order App</h1>
     </>
   );
